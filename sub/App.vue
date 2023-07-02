d83745ba077f9b1fd5bb09a6d0ea49a7a0a0b94e
Thy <uipi@foxmail.com>
2023-07-02 13:47:35 +0800

    feat: sub page back btn

--- sub/App.vue
+++ sub/App.vue
@@ -14,7 +14,7 @@ export default defineComponent({
     <div>
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>Sub App</h1>
-        <a href="/">返回主页面</a>
+        <a href="/">Back to main page</a>
         <p>Vue version: {{ version }}</p>
     </div>
 </template>
